639bf1b390bf0e6928cc108c2167e1d6d27032a9
Gideon Groeneveld <g.p.a.groeneveld@student.tudelft.nl>
2024-03-01 18:11:09 +0100

    Rewritten into sections

--- Mathematical_MPC.m
+++ Mathematical_MPC.m
@@ -1,3 +1,31 @@
+%% Run Simulation
+N_steps = 200;
+
+% Initialize arrays to store results
+x_results = zeros(2, N_steps);
+u_results = zeros(1, N_steps);
+
+% Initialize state
+x_k = x0;
+
+for i = 1:N_steps
+    % Obtain optimal control action
+    [u_k, info] = mpcmove(mpcobj, xc);
+    
+    % Apply first input to the system
+    x_k = A1*x_k + B1*u_k;
+    
+    % Update the measured state
+    xc.Plant = x_k;
+    
+    % Store results
+    x_results(:, i) = x_k;
+    u_results(i) = u_k;
+
+    % Display results
+    % disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
+end
+
 %% Plot results
 figure;
 subplot(2, 1, 1);
@@ -18,6 +46,9 @@ title('Control Input Evolution');
 grid on;
 
 %% Exercise 1.1: Initialize
+% Time Step
+k = 1;
+
 % Define system matrices
 A1 = [1, 0.1; 0, 1];
 B1 = [0; 0.1];
@@ -30,13 +61,6 @@ M = 1;
 
 % Define initial condition
 x0 = [10; 0];
-
-% Define number of states and inputs
-n_states = size(A1, 1);
-n_inputs = size(B1, 2);
-
-% Time Step
-k = 1;
 
 % Create LTI model mpcobject
 plant = ss(A1, B1, C1, D1, k);
@@ -53,30 +77,8 @@ R = 1;      % Input weight
 mpcobj.Weights.ManipulatedVariables = {R};
 mpcobj.Weights.OutputVariables = {Q};
 
-% Initialize states
-x_k = x0;
-
 % Create MPCSTATE mpcobject
 xc = mpcstate(mpcobj);
-
-% Initialize arrays to store results
-x_results = zeros(2, 200);
-u_results = zeros(1, 200);
-
-for i = 1:200 % Run for 200 time steps
-    % Obtain optimal control action
-    [u_k, info] = mpcmove(mpcobj, xc);
-    
-    % Apply first input to the system
-    x_k = A1*x_k + B1*u_k;
-    
-    % Update the measured state
-    xc.Plant = x_k;
-    
-    % Store results
-    x_results(:, i) = x_k;
-    u_results(i) = u_k;
-end
 
 %% Exercise 1.2: Change Q = 100I
 Q = 10 * eye(2); % State weight matrix
@@ -85,74 +87,23 @@ Q = 10 * eye(2); % State weight matrix
 mpcobj.Weights.ManipulatedVariables = {R};
 mpcobj.Weights.OutputVariables = {Q};
 
-% Initialize states
-x_k = x0;
-
 % Create MPCSTATE mpcobject
 xc = mpcstate(mpcobj);
-
-for i = 1:200 % Run for 20 time steps
-    % Obtain optimal control action
-    [u_k, info] = mpcmove(mpcobj, xc);
-    
-    % Apply first input to the system
-    x_k = A1*x_k + B1*u_k;
-    
-    % Update the measured state
-    xc.Plant = x_k;
-    
-    % Display results
-    disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
-end
 
 %% Exercise 1.3: Include input constraint |uk| < 1
 % Set input constraints
 mpcobj.MV(1).Min = -1;
 mpcobj.MV(1).Max = 1;
 
-% Initialize states
-x_k = x0;
-
 % Create MPCSTATE mpcobject
 xc = mpcstate(mpcobj);
-
-for i = 1:20 % Run for 20 time steps
-    % Obtain optimal control action
-    [u_k, info] = mpcmove(mpcobj, xc);
-    
-    % Apply first input to the system
-    x_k = A1*x_k + B1*u_k;
-    
-    % Update the measured state
-    xc.Plant = x_k;
-    
-    % Display results
-    disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
-end
 
 %% Exercise 1.4: Include state constraint x_k[1] > -2
 % Set state constraints
 mpcobj.OV(1).Min = -2;
 
-% Initialize states
-x_k = x0;
-
 % Create MPCSTATE mpcobject
 xc = mpcstate(mpcobj);
-
-for i = 1:20 % Run for 20 time steps
-    % Obtain optimal control action
-    [u_k, info] = mpcmove(mpcobj, xc);
-    
-    % Apply first input to the system
-    x_k = A1*x_k + B1*u_k;
-    
-    % Update the measured state
-    xc.Plant = x_k;
-    
-    % Display results
-    disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
-end
 
 %% Exercise 1.5: Increase prediction horizon to Np = 100
 % Define prediction horizon and control horizon
@@ -173,35 +124,16 @@ mpcobj.MV(1).Max = 1;
 % Set state constraint
 mpcobj.OV(1).Min = -2;
 
-% Initialize states
-x_k = x0;
-
 % Create MPCSTATE mpcobject
 xc = mpcstate(mpcobj);
 
-for i = 1:20 % Run for 20 time steps
-    % Obtain optimal control action
-    [u_k, info] = mpcmove(mpcobj, xc);
-    
-    % Apply first input to the system
-    x_k = A1*x_k + B1*u_k;
-    
-    % Update the measured state
-    xc.Plant = x_k;
-    
-    % Display results
-    disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
-end
-
 %% Exercise 1.6: Include terminal constraint x_k+Np[2] = 0
 % Define prediction horizon and control horizon
-
 P = 10;
 M = 1;
 
 % Create MPC controller mpcobject
 mpcobj = mpc(plant, P, M);
-mpcobj.PredictionHorizon = 8;
 
 % Set cost function weights
 mpcobj.Weights.ManipulatedVariables =  {R};
@@ -222,23 +154,8 @@ mpcobj.OV(1).Min = -2;
 %setterminal(mpcobj, TerminalConstr);
 % Include terminal constraint
 %setterminal = mpcobj
-% Initialize states
-x_k = x0;
 
 % Create MPCSTATE mpcobject
 xc = mpcstate(mpcobj);
 
-for i = 1:20 % Run for 20 time steps
-    % Obtain optimal control action
-    [u_k, info] = mpcmove(mpcobj, xc);
-    
-    % Apply first input to the system
-    x_k = A1*x_k + B1*u_k;
-    
-    % Update the measured state
-    xc.Plant = x_k;
-    
-    % Display results
-    disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
-end
-
+%% Excercise 1.7: 

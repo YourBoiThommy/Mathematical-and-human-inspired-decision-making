db96f19144509c34c6c9f02e3298594830c1d472
Gideon Groeneveld <g.p.a.groeneveld@student.tudelft.nl>
2024-03-01 17:53:50 +0100

    Plotting section

--- Mathematical_MPC.m
+++ Mathematical_MPC.m
@@ -1,3 +1,23 @@
+%% Plot results
+figure;
+subplot(2, 1, 1);
+plot(1:200, x_results(1, :), 'b', 'LineWidth', 1.5);
+hold on;
+plot(1:200, x_results(2, :), 'r', 'LineWidth', 1.5);
+xlabel('Time Step');
+ylabel('State Value');
+title('State Evolution');
+legend('x_1', 'x_2');
+grid on;
+
+subplot(2, 1, 2);
+plot(1:200, u_results, 'k', 'LineWidth', 1.5);
+xlabel('Time Step');
+ylabel('Control Input');
+title('Control Input Evolution');
+grid on;
+
+%% Exercise 1.1: Initialize
 % Define system matrices
 A1 = [1, 0.1; 0, 1];
 B1 = [0; 0.1];
@@ -58,26 +78,7 @@ for i = 1:200 % Run for 200 time steps
     u_results(i) = u_k;
 end
 
-% Plot results
-figure;
-subplot(2, 1, 1);
-plot(1:200, x_results(1, :), 'b', 'LineWidth', 1.5);
-hold on;
-plot(1:200, x_results(2, :), 'r', 'LineWidth', 1.5);
-xlabel('Time Step');
-ylabel('State Value');
-title('State Evolution');
-legend('x_1', 'x_2');
-grid on;
-
-subplot(2, 1, 2);
-plot(1:200, u_results, 'k', 'LineWidth', 1.5);
-xlabel('Time Step');
-ylabel('Control Input');
-title('Control Input Evolution');
-grid on;
-%%
-% Exercise 1.2: Change Q = 100I
+%% Exercise 1.2: Change Q = 100I
 Q = 10 * eye(2); % State weight matrix
 
 % Set cost function weights
@@ -104,8 +105,7 @@ for i = 1:200 % Run for 20 time steps
     disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
 end
 
-%%
-% Exercise 1.3: Include input constraint |uk| < 1
+%% Exercise 1.3: Include input constraint |uk| < 1
 % Set input constraints
 mpcobj.MV(1).Min = -1;
 mpcobj.MV(1).Max = 1;
@@ -130,9 +130,7 @@ for i = 1:20 % Run for 20 time steps
     disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
 end
 
-%%
-
-% Exercise 1.4: Include state constraint x_k[1] > -2
+%% Exercise 1.4: Include state constraint x_k[1] > -2
 % Set state constraints
 mpcobj.OV(1).Min = -2;
 
@@ -156,9 +154,7 @@ for i = 1:20 % Run for 20 time steps
     disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
 end
 
-%%
-
-% Exercise 1.5: Increase prediction horizon to Np = 100
+%% Exercise 1.5: Increase prediction horizon to Np = 100
 % Define prediction horizon and control horizon
 Np = 100;
 M = 1;
@@ -197,8 +193,7 @@ for i = 1:20 % Run for 20 time steps
     disp(['x_k:', num2str(x_k'), ', u_k:', num2str(u_k')]);
 end
 
-%%
-% Exercise 1.6: Include terminal constraint x_k+Np[2] = 0
+%% Exercise 1.6: Include terminal constraint x_k+Np[2] = 0
 % Define prediction horizon and control horizon
 
 P = 10;
